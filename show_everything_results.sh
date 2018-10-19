3f1b76cd6d4834311195f84231b54395f1ffbae1
Dirk Groeneveld <dirkg@allenai.org>
2018-10-19 14:24:33 -0700

    Sort the experiments properly

--- show_everything_results.sh
+++ show_everything_results.sh
@@ -6,8 +6,8 @@ set -e
 
 ls -1 $EXPERIMENT/*/metrics.json | \
 parallel 'echo {} $(jq .test_accuracy_avg {})' | \
-sort | \
 sed -e "s/\/metrics.json//" | \
 sed -Ee 's/\.json_([0-9][0-9]-){6}r[0-9]+//' | \
 sed -e "s/^[^ ]*\/\+//" | \
+sort | \
 csvtool -t " " -u TAB transpose -
